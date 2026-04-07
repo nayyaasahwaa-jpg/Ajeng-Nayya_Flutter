061864ac8886d6a39cd9e4df7b960b0ad40437fa
nayyaasahwaa-jpg <nayyaasahwaa@gmail.com>
2026-04-07 07:24:11 -0700

    Add files via upload

---
+++ lib/produk_detail.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ProdukDetail extends StatefulWidget {
+  final String kodeProduk;
+  final String namaProduk;
+  final int harga;
+
+  ProdukDetail({
+    required this.kodeProduk,
+    required this.namaProduk,
+    required this.harga,
+  });
+
+  @override
+  _ProdukDetailState createState() => _ProdukDetailState();
+}
+
+class _ProdukDetailState extends State<ProdukDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Detail Produk")),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Kode Produk : ${widget.kodeProduk}"),
+            SizedBox(height: 10),
+            Text("Nama Produk : ${widget.namaProduk}"),
+            SizedBox(height: 10),
+            Text("Harga : ${widget.harga}"),
+          ],
+        ),
+      ),
+    );
+  }
+}
